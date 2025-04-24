10ddc387a00ebe16c810325ad340761bfc9ce30a
CarolineNkan <145355980+CarolineNkan@users.noreply.github.com>
2025-04-24 04:57:31 -0400

    changed projects section title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,28 +10,28 @@ export default function Navbar() {
       </a>
       <a
         href="#projects"
-        className="hover:text-netflix font-semibold transition"
+  className="hover:text-netflix font-semibold transition"
         aria-label="Go to Projects section"
       >
         Projects
       </a>
       <a
         href="#stories"
-        className="hover:text-netflix font-semibold transition"
+  className="hover:text-netflix font-semibold transition"
         aria-label="Go to Strategic Stories section"
       >
         Strategic Stories
       </a>
       <a
         href="#impact"
-        className="hover:text-netflix font-semibold transition"
+  className="hover:text-netflix font-semibold transition"
         aria-label="Go to Real-World Impact section"
       >
         Real-World Impact
       </a>
       <a
         href="#footer"
-        className="hover:text-netflix font-semibold transition"
+  className="hover:text-netflix font-semibold transition"
         aria-label="Go to Footer"
       >
         Connect
